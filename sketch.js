72a24bfec53a0d70dc43d428fd22f29c7279fb30
manucherukumille <102837044+manucherukumille@users.noreply.github.com>
2022-04-01 16:56:32 -0700

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,29 @@
+var box 
+
+function setup() {
+  createCanvas(400,400);
+
+  box = createSprite(200, 200, 20, 20)
+}
+
+function draw() 
+{
+  background(30);
+  
+  if(keyIsDown(RIGHT_ARROW)){
+    box.x+=10
+  }
+
+  if(keyIsDown(LEFT_ARROW)){
+    box.x-=10
+  }
+
+
+
+  drawSprites()
+  
+}
+
+
+
+
